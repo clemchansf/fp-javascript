fc9e3a2407f9643006aaa6409f3b74feef1c75a1
Clem <clem.chan.sf@gmail.com>
2021-08-20 13:47:07 -0700

    Add incomplete form data test

--- src/form-validator.test.js
+++ src/form-validator.test.js
@@ -1,5 +1,25 @@
+import { expect } from 'chai';
+import { getErrorMessage, inputCriteria } from './form-validator';
+
 describe('Form Field Validation - basic functionality', () => {
-  it('gets error messages for a incomplete form data');
+  it('gets error messages for a incomplete form data', () => {
+    const currentInputValues = {
+      firstName: '', // Must be at least 2 characters
+      lastName: '', // Must be at least 2 characters
+      zipCode: '', // Must be exactly 5 characters
+      state: '' // Must be exactly 2 characters
+    };
+
+    const expected = [
+      'First name must be at least 2 characters',
+      'Last name must be at least 2 characters',
+      'Zip code must be exactly 5 characters',
+      'State must be exactly 2 characters'
+    ];
+
+    const actual = getErrorMessage(currentInputValues, inputCriteria);
+    expect(actual).to.deep.equal(expected);
+  });
   it('gets error messages for a partially completed form data');
   it('gets error messages for a completed form data');
 });
