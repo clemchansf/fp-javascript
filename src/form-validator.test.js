a89bddd085faf6829027f81cef832a26958b43df
Clem <clem.chan.sf@gmail.com>
2021-08-20 15:43:11 -0700

    add partial input test

--- src/form-validator.test.js
+++ src/form-validator.test.js
@@ -20,6 +20,20 @@ describe('Form Field Validation - basic functionality', () => {
     const actual = getErrorMessage(currentInputValues, inputCriteria);
     expect(actual).to.deep.equal(expected);
   });
-  it('gets error messages for a partially completed form data');
+  it('gets error messages for a partially completed form data', () => {
+    const partialInputValues = {
+      firstName: 'Good', // Must be at least 2 characters
+      lastName: 'Morning', // Must be at least 2 characters
+      zipCode: '', // Must be exactly 5 characters
+      state: '' // Must be exactly 2 characters
+    };
+
+    const expected = [
+      'Zip code must be exactly 5 characters',
+      'State must be exactly 2 characters'
+    ];
+    const actual = getErrorMessage(partialInputValues);
+    expect(actual).to.deep.equal(expected);
+  });
   it('gets error messages for a completed form data');
 });
